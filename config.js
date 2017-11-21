aa1ceac15219559f8f581f4c01f82e4ce26fd01b
Chris <iamchris88@live.co.uk>
2017-11-21 11:05:14 +0000

    a

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 var config = {}
 
 config.databaseURL = 'mongodb://messageapp:mapp1@ds137435.mlab.com:37435/message-service';
-config.orderServiceURLtoUpdateWithPurchase = "http://localhost:3001/purchasing/test"; //"http://localhost:3004/PurchasingUpdate";
-config.testDatabaseURL = "mongodb://localhost:27017/MyDb";
-config.AdminServicePurchaseURL = "http://localhost:3001/purchasing/test";
-config.stockServiceUpdaterURL= "http://localhost:3001/purchasing/test";
+config.orderServiceURLtoUpdateWithPurchase = "http://localhost:3001/purchasing/test"; 
 module.exports = config;
